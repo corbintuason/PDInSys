ddfce944876609d4e122f8c7c466baa3eabbce8d
ian.halcyondigital <mivgerado.halcyondigital@gmail.com>
2020-03-16 21:51:33 +0800

    login page update
    Signed-off-by: ian.halcyondigital <mivgerado.halcyondigital@gmail.com>

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -11,53 +11,73 @@
     <div>
         <div class="tile is-parent">
             <div class="tile is-parent is-4">
-                <b-card header-tag="header" footer-tag="footer" class="card">
-                    <template v-slot:header>
-                        <h4 class="mb-0">
-                            Welcome to Project Duo Information System
-                        </h4>
-                    </template>
-                    <b-form>
-                        <b-form-group
-                            label="Email address:"
-                            label-for="input-1"
-                        >
-                            <b-form-input
-                                type="email"
-                                required
-                                placeholder="Enter email"
-                            ></b-form-input>
-                        </b-form-group>
-
-                        <b-form-group label="Password:" label-for="input-2">
-                            <b-form-input
-                                type="password"
-                                required
-                                placeholder="Enter Password"
-                            ></b-form-input>
-                        </b-form-group>
-                    </b-form>
-
-                    <div class="row">
-                        <div class="col-md-8">
-                            <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0"
-                                >Remember me</b-form-checkbox
-                            >
-                        </div>
-                        <div class="col-md-4 text-right">
-                            <b-link href="#foo">Forgot Password?</b-link>
-                        </div>
+                <div class="card row card-holder">
+                    <div class="col-md-6 left-side-login">
+                        <!-- <b-card class="image-login-holder"> -->
+                        <img
+                            class="image-login"
+                            src="/img/login/login.png"
+                            width="400"
+                            height="230"
+                        />
+                        <!-- </b-card> -->
                     </div>
 
-                    <template v-slot:footer>
-                        <b-button block href="#" variant="primary"
-                            >Login</b-button
-                        >
-                    </template>
-                </b-card>
+                    <div class="col-md-6 right-side-login">
+                        <div class="header-login">
+                            <h3 class="font-weight-bold">Login</h3>
+                        </div>
+
+                        <div class="sub-header-login">
+                            <h6 class="font-italic">
+                                Welcome to Project Duo Information System
+                            </h6>
+                        </div>
+
+                        <b-form>
+                            <b-form-group
+                                label="Email address:"
+                                label-for="input-1"
+                            >
+                                <b-form-input
+                                    type="email"
+                                    required
+                                    placeholder="Enter email"
+                                ></b-form-input>
+                            </b-form-group>
+
+                            <b-form-group label="Password:" label-for="input-2">
+                                <b-form-input
+                                    type="password"
+                                    required
+                                    placeholder="Enter Password"
+                                ></b-form-input>
+                            </b-form-group>
+                        </b-form>
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0"
+                                    >Remember me</b-form-checkbox
+                                >
+                            </div>
+                            <div class="col-md-6 text-right">
+                                <b-link href="#foo">Forgot Password?</b-link>
+                            </div>
+                        </div>
+
+                        <div class="button-login">
+                            <b-button block variant="primary font-weight-bold"
+                                >Login</b-button
+                            >
+                        </div>
+                    </div>
+                    <!-- card card-holder div -->
+                </div>
+                <!-- tile div -->
             </div>
         </div>
-
+        <!-- vue particles -->
         <vue-particles id="particles-js"> </vue-particles>
     </div>
 </template>
@@ -76,9 +96,22 @@ export default {
 <style lang="scss" scoped>
 * {
     box-sizing: border-box;
+    font-family: "Lato";
 }
 .card {
     border-radius: 10px;
+    box-shadow: 0 2px 43px -4px rgba(0, 0, 0, 0.19) !important;
+    transition: all 0.2s ease-out;
+
+    &:hover {
+        transform: translateY(4px);
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3), 0 1px 2px rgba(0, 0, 0, 0.08) !important;
+    }
+}
+.card-holder {
+    width: 930px;
+    height: 450px;
+    // background: #eff2f7;
 }
 .tile {
     position: relative;
@@ -91,6 +124,27 @@ export default {
         align-items: center;
         justify-content: center;
         margin: auto;
+    }
+    .left-side-login {
+        background: #eff2f7;
+        padding-bottom: 118px;
+        padding-left: 20px;
+        border-radius: 10px 0 0 10px;
+    }
+    .right-side-login {
+        padding-top: 30px;
+    }
+    .header-login {
+        margin-bottom: 15px;
+    }
+    .sub-header-login {
+        margin-bottom: 40px;
+    }
+    .image-login {
+        margin-top: 100px;
+    }
+    .button-login {
+        margin-top: 55px;
     }
 }
 #particles-js {
