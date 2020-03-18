d16176c22fa3d00e7b74c205ca8df4c3503bb7e8
ian.halcyondigital <mivgerado.halcyondigital@gmail.com>
2020-03-18 22:01:12 +0800

    update welcome user
    Signed-off-by: ian.halcyondigital <mivgerado.halcyondigital@gmail.com>

--- resources/js/components/authorized/dashboard/WelcomeUser.vue
+++ resources/js/components/authorized/dashboard/WelcomeUser.vue
@@ -1,20 +1,53 @@
 <template>
-  <b-card class="text-center">
-    <b-card-text>
-      <img src="img_avatar.png" style="border-radius:50%; border:solid 1px; " alt="Avatar" />
-    </b-card-text>
+    <b-card class="text-center welcome-card">
+        <b-card-text>
+            <b-img
+                class="mt-4"
+                src="/img/avatar/user.png"
+                rounded="circle"
+                width="60px"
+                height="60px"
+                alt="Photo"
+            ></b-img>
+        </b-card-text>
 
-    <b-card-text>Welcome Back <strong>{{ user.meta.full_name }}! </strong> </b-card-text>
-  </b-card>
+        <b-card-text
+            ><div class="mb-2">
+                <strong>
+                    Welcome Back,
+                    {{ user.meta.full_name }}
+                </strong>
+            </div>
+            <div class="mt-1 ml-1">
+                <h5 class="text-uppercase font-weight-bold">
+                    {{ user.data.group }}
+                </h5>
+            </div>
+        </b-card-text>
+    </b-card>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      user: this.$store.state.user
-    };
-  },
-  props: {}
+    data() {
+        return {
+            user: this.$store.state.user
+        };
+    },
+    props: {}
 };
 </script>
+
+<style lang="scss" scoped>
+.welcome-card {
+    // background: linear-gradient(90deg, #6871b5, #9c8ef3, #6871b5);
+    border-radius: 10px;
+    box-shadow: 0 2px 43px -4px rgba(0, 0, 0, 0.08) !important;
+    transition: all 0.2s ease-out;
+
+    &:hover {
+        transform: translateY(4px);
+        box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.11) !important;
+    }
+}
+</style>
