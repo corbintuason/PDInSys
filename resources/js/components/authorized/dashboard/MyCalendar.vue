19068422bf6946b479333e126ac7590a77a82daa
ian.halcyondigital <mivgerado.halcyondigital@gmail.com>
2020-03-19 21:10:41 +0800

    my calendar first commit
    Signed-off-by: ian.halcyondigital <mivgerado.halcyondigital@gmail.com>

---
+++ resources/js/components/authorized/dashboard/MyCalendar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <b-card class="calendar-card">
+            <div class="column content--column">
+                <FullCalendar
+                    defaultView="dayGridMonth"
+                    :plugins="calendarPlugins"
+                    :custom-buttons="customButtons"
+                    :header="header"
+                    :height="500"
+                    :footer="footer"
+                />
+            </div>
+        </b-card>
+    </div>
+</template>
+
+<script>
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+export default {
+    components: { FullCalendar },
+    data() {
+        return {
+            calendarEvents: [{ title: "Event Now", start: new Date() }],
+            calendarPlugins: [dayGridPlugin],
+            height: 0,
+            aspectRatio: 0,
+            contentHeight: 0,
+            selectable: true,
+            header: {
+                right: "dayGridMonth,dayGridWeek,dayGridDay",
+                left: "title"
+            },
+            footer: {
+                center: "prev,today,next"
+            }
+        };
+    }
+};
+</script>
+
+<style lang="scss">
+@import "~@fullcalendar/core/main.css";
+@import "~@fullcalendar/daygrid/main.css";
+
+.calendar-card {
+    border-radius: 10px;
+}
+</style>
--- resources/js/pages/Authorized/Dashboard.vue
+++ resources/js/pages/Authorized/Dashboard.vue
@@ -1,30 +1,32 @@
 <template>
-  <div>
-      <!-- Welcome -->
-      <div class="row mt-3">
-        <div class="col-md-4">
-          <welcome-user></welcome-user>
-        </div>
-        <!-- Advisories -->
+    <div>
+        <!-- Welcome -->
+        <div class="row mt-3">
+            <div class="col-md-4">
+                <welcome-user></welcome-user>
+            </div>
+            <!-- Advisories -->
 
-        <div class="col-md-8">
-          <notification-center></notification-center>
+            <div class="col-md-8">
+                <notification-center></notification-center>
+            </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <!-- Happening Now -->
-          <!-- <happening-now></happening-now> -->
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Happening Now -->
+                <!-- <happening-now></happening-now> -->
+            </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="col-md-12">
-          <!-- My Schedule -->
-          <my-schedule></my-schedule>
+        <div class="row">
+            <div class="col-md-4">
+                <my-calendar></my-calendar>
+            </div>
+            <div class="col-md-6">
+                <!-- My Schedule -->
+            </div>
         </div>
-      </div>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -32,23 +34,24 @@ import adminNav from "../../components/authorized/navigation/AdminNav";
 import welcomeUser from "../../components/authorized/dashboard/WelcomeUser";
 import notificationCenter from "../../components/authorized/dashboard/NotificationCenter";
 import happeningNow from "../../components/authorized/dashboard/HappeningNow";
-import mySchedule from "../../components/authorized/dashboard/MySchedule";
+import myCalendar from "../../components/authorized/dashboard/MyCalendar";
 
 export default {
-  data() {
-    return {
-      user: this.$store.state.user
-    };
-  },
-  components: {
-    "admin-nav": adminNav,
-    "welcome-user": welcomeUser,
-    "notification-center": notificationCenter,
-    "happening-now": happeningNow,
-    "my-schedule": mySchedule
-  },
-  computed: {},
-  methods: {},
-  mounted() {}
+    data() {
+        return {
+            user: this.$store.state.user
+        };
+    },
+    components: {
+        "admin-nav": adminNav,
+        "welcome-user": welcomeUser,
+        "notification-center": notificationCenter,
+        "happening-now": happeningNow,
+        "my-calendar": myCalendar
+        // "my-schedule": mySchedule
+    },
+    computed: {},
+    methods: {},
+    mounted() {}
 };
 </script>
