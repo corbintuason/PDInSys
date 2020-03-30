8286de249c037ad5277b0b3628716fa900c90288
ian.halcyondigital <mivgerado.halcyondigital@gmail.com>
2020-03-30 15:16:58 +0800

    init
    Signed-off-by: ian.halcyondigital <mivgerado.halcyondigital@gmail.com>

--- resources/js/pages/Authorized/Dashboard.vue
+++ resources/js/pages/Authorized/Dashboard.vue
@@ -1,40 +1,44 @@
 <template>
-    <div>
-        <!-- Welcome -->
-        <div class="row mt-3">
-            <div class="col-md-4">
-                <welcome-user></welcome-user>
-            </div>
-            <!-- Advisories -->
+  <div>
+    <!-- Welcome -->
+    <div class="row mt-3">
+      <div class="col-md-4">
+        <welcome-user></welcome-user>
+      </div>
+      <!-- Advisories -->
 
-            <div class="col-md-8">
-                <notification-center></notification-center>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <!-- Happening Now -->
-                <!-- <happening-now></happening-now> -->
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <!-- My Calendar -->
-                <my-calendar></my-calendar>
-            </div>
-            <div class="col-md-8">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div><happening-now></happening-now></div>
-                    </div>
-                    <div class="col-md-6">
-                        <div><company-advisories></company-advisories></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div class="col-md-8">
+        <notification-center></notification-center>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-md-12">
+        <!-- Happening Now -->
+        <!-- <happening-now></happening-now> -->
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4">
+        <!-- My Calendar -->
+        <my-calendar></my-calendar>
+      </div>
+      <div class="col-md-8">
+        <div class="row">
+          <div class="col-md-6">
+            <div>
+              <happening-now></happening-now>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div>
+              <company-advisories></company-advisories>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
