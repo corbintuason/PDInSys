0d3f664c040964dc8392f79710ce726dac34aaf6
John Corbin Tuason <johncorbintuason@gmail.com>
2020-05-13 01:44:58 +0800

    fix
    Signed-off-by: John Corbin Tuason <johncorbintuason@gmail.com>

--- resources/js/pages/Authorized/PGAS/BudgetAllocation/Create/AllCostCenter.vue
+++ resources/js/pages/Authorized/PGAS/BudgetAllocation/Create/AllCostCenter.vue
@@ -43,73 +43,6 @@ export default {
 	data() {
 		return {
 			tabIndex: 0,
-			budget: {
-				adm_cost_centers: [
-					{
-						adm_cost_code: "",
-						adm_cost_description: "",
-						adm_budget_details: [
-							{
-								adm_budget_code: "",
-								adm_budget_description: "",
-								adm_budget_year: null
-							}
-						]
-					}
-				],
-				acc_cost_centers: [
-					{
-						acc_cost_code: "",
-						acc_cost_description: "",
-						acc_budget_details: [
-							{
-								acc_budget_code: "",
-								acc_budget_description: "",
-								acc_budget_year: null
-							}
-						]
-					}
-				],
-				bod_cost_centers: [
-					{
-						bod_cost_code: "",
-						bod_cost_description: "",
-						bod_budget_details: [
-							{
-								bod_budget_code: "",
-								bod_budget_description: "",
-								bod_budget_year: null
-							}
-						]
-					}
-				],
-				hum_cost_centers: [
-					{
-						hum_cost_code: "",
-						hum_cost_description: "",
-						hum_budget_details: [
-							{
-								hum_budget_code: "",
-								hum_budget_description: "",
-								hum_budget_year: null
-							}
-						]
-					}
-				],
-				ops_cost_centers: [
-					{
-						ops_cost_code: "",
-						ops_cost_description: "",
-						ops_budget_details: [
-							{
-								ops_budget_code: "",
-								ops_budget_description: "",
-								ops_budget_year: null
-							}
-						]
-					}
-				]
-			}
 		};
 	},
 	components: {
@@ -118,6 +51,9 @@ export default {
 		"bod-cost-center": bodCostCenter,
 		"hum-cost-center": humCostCenter,
 		"ops-cost-center": opsCostCenter
+	},
+	props:{
+		budget: Object
 	}
 };
 </script>
