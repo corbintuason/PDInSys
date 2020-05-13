4a059322699ef9eec389aa3ef0253d02a9bceaab
Mark Ian Gerado <mivgerado.halcyon>
2020-05-13 15:49:46 +0800

    update ba
    Signed-off-by: Mark Ian Gerado <mivgerado.halcyon>

--- resources/js/pages/Authorized/PGAS/BudgetAllocation/Create/Budget/AnnualBudget.vue
+++ resources/js/pages/Authorized/PGAS/BudgetAllocation/Create/Budget/AnnualBudget.vue
@@ -7,7 +7,7 @@
 						<h4>
 							<strong>
 								<b-input-group prepend="₱" class="font-weight-bold">
-									<!-- <money readonly class="form-control" :value="annual_grand_total"></money> -->
+									<money readonly class="form-control" :value="annual_grand_total"></money>
 								</b-input-group>
 							</strong>
 						</h4>
@@ -32,8 +32,28 @@ export default {
 		budget: Array
 	},
 	computed: {
-		// annual_grand_total() {
-		// }
+		annual_grand_total() {
+			var total_amount = 0;
+			var adm_total = 0;
+			var acc_total = 0;
+
+			this.budget.forEach(center => {
+				center.adm_budget_details.forEach(detail => {
+					adm_total += Number(detail.adm_budget_year);
+				});
+				console.log("total amount is", adm_total);
+			});
+			this.budget.forEach(center => {
+				center.acc_budget_details.forEach(detail => {
+					acc_total += Number(detail.acc_budget_year);
+				});
+				console.log("total amount is", acc_total);
+			});
+
+			this.total_amount = this.adm_total + this.acc_total;
+
+			return total_amount;
+		}
 	},
 	data() {
 		return {};
