f4025589b2b66918c46ea39dcb8020c6a022ccc1
Mark Ian Gerado <mivgerado.halcyondigital@gmail.com>
2020-05-23 19:20:47 +0800

    create mandate update
    Signed-off-by: Mark Ian Gerado <mivgerado.halcyondigital@gmail.com>

--- resources/js/pages/Authorized/PGAS/Mandate/Create/CreateMandate.vue
+++ resources/js/pages/Authorized/PGAS/Mandate/Create/CreateMandate.vue
@@ -1,189 +1,203 @@
 <template>
-	<div>
-		<b-card class="mt-3">
-			<template v-slot:header>
-				<h1 class="component-title">Project Based Info Sheet</h1>
-			</template>
+    <div>
+        <b-card class="mt-3">
+            <template v-slot:header>
+                <h1 class="component-title">Project Based Info Sheet</h1>
+            </template>
 
-			<b-card-text>
-				<b-tabs v-model="tabIndex" content-class="mt-3" fill>
-					<b-tab title="General Info" active>
-						<general-info :mandate="mandate"></general-info>
-					</b-tab>
-					<b-tab title="Government Details">
-						<government-details :mandate="mandate"></government-details>
-					</b-tab>
-					<b-tab title="Education Attainment and Work Experience">
-						<education-work :mandate="mandate"></education-work>
-					</b-tab>
-					<b-tab title="Other Information">
-						<other-information :mandate="mandate"></other-information>
-					</b-tab>
-				</b-tabs>
-			</b-card-text>
-			<template v-slot:footer>
-				<div class="row">
-					<div class="col-md-6">
-						<b-button-group class="mt-1">
-							<b-button @click="tabIndex--">Previous Tab</b-button>
-							<b-button @click="tabIndex++">Next Tab</b-button>
-						</b-button-group>
-					</div>
-					<div class="col-md-6 text-right">
-						<b-button
-							:disabled="isDisabled"
-							variant="success"
-							class="float-right"
-							@click="createMandate"
-						>Create Mandate</b-button>
-					</div>
-				</div>
-			</template>
-		</b-card>
-	</div>
+            <b-card-text>
+                <b-tabs v-model="tabIndex" content-class="mt-3" fill>
+                    <b-tab title="General Info" active>
+                        <general-info :mandate="mandate"></general-info>
+                    </b-tab>
+                    <b-tab title="Government Details">
+                        <government-details
+                            :mandate="mandate"
+                        ></government-details>
+                    </b-tab>
+                    <b-tab title="Education Attainment and Work Experience">
+                        <education-work :mandate="mandate"></education-work>
+                    </b-tab>
+                    <b-tab title="Other Information">
+                        <other-information
+                            :mandate="mandate"
+                        ></other-information>
+                    </b-tab>
+                </b-tabs>
+            </b-card-text>
+            <template v-slot:footer>
+                <div class="row">
+                    <div class="col-md-6">
+                        <b-button-group class="mt-1">
+                            <b-button @click="tabIndex--"
+                                >Previous Tab</b-button
+                            >
+                            <b-button @click="tabIndex++">Next Tab</b-button>
+                        </b-button-group>
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <b-button
+                            :disabled="isDisabled"
+                            variant="success"
+                            class="float-right"
+                            @click="createMandate"
+                            >Create Mandate</b-button
+                        >
+                    </div>
+                </div>
+            </template>
+        </b-card>
+    </div>
 </template>
 
 <script>
+import form from "../../../../../mixins/form";
+import accountModule from "../../../../../store/modules/mandate";
 import generalInfo from "./components/GeneralInfo";
 import governmentDetails from "./components/GovernmentDetails";
 import otherInformation from "./components/OtherInformation";
 import educationWork from "./components/EducationWork";
-import form from "../../../../../mixins/form";
+import { mapGetters, mapState } from "vuex";
+
 export default {
-	props: {
-		steps: Array,
-		endpoints: Object
-	},
-	mixins: [form],
-	data() {
-		return {
-			name: "Create Mandate",
-			user: this.$store.state.user,
-			tabIndex: 0,
-			mandate: {
-				date: "",
-				position: "",
-				full_name: {
-					last_name: "",
-					first_name: "",
-					middle_name: ""
-				},
-				region: "",
-				permanent_address: "",
-				present_address: "",
-				gender: "",
-				civil_status: "",
-				birthdate: "",
-				age: "",
-				mobile_number: "",
-				telephone_number: "",
-				religion: "",
-				sss_number: "",
-				tin_number: "",
-				pagibig_number: "",
-				philhealth_number: "",
-				passport_number: "",
-				tertiary_details: {
-					tertiary_name: "",
-					tertiary_date: ""
-				},
-				secondary_details: {
-					secondary_name: "",
-					secondary_date: ""
-				},
-				primary_details: {
-					primary_name: "",
-					primary_date: ""
-				},
-				work_details: [
-					{
-						work_position: "",
-						job_description: "",
-						start_date: "",
-						end_date: ""
-					}
-				],
-				father_details: {
-					father_name: "",
-					father_occupation: ""
-				},
-				mother_details: {
-					mother_name: "",
-					mother_occupation: ""
-				},
-				spouse_details: {
-					spouse_name: "",
-					spouse_occupation: ""
-				},
-				emergency_details: [
-					{
-						contact_person: "",
-						contact_number: ""
-					}
-				]
-			}
-		};
-	},
-	components: {
-		"general-info": generalInfo,
-		"government-details": governmentDetails,
-		"other-information": otherInformation,
-		"education-work": educationWork
-	},
-	computed: {
-		get_status() {
-			if (this.$store.getters.hasAbility("approve-all-mandates")) {
-				return "For Approval";
-			}
-		},
-		isDisabled() {
-			if (this.mandate.position == "" || this.mandate.full_name == "") {
-				return true;
-			}
-			if (
-				this.mandate.present_address == "" ||
-				this.mandate.permanent_address == ""
-			) {
-				return true;
-			}
-			if (
-				this.mandate.mobile_number == "" ||
-				this.mandate.telephone_number == ""
-			) {
-				return true;
-			}
-			if (
-				this.mandate.tin_number == "" ||
-				this.mandate.sss_number == ""
-			) {
-				return true;
-			}
-			if (
-				this.mandate.pagibig_number == "" ||
-				this.mandate.philhealth_number == ""
-			) {
-				return true;
-			}
-		}
-	},
-	methods: {
-		createMandate() {
-			this.mandate.status = this.get_status;
-			var swal_html = this.loadSwalContents(this.steps, this.user);
-			const swal_object = {
-				title: "Create Mandate",
-				html: swal_html,
-				text: "Please check the details provided.",
-				confirmButtonText: "Create Mandate",
-				item: this.mandate,
-				endpoints: this.endpoints
-			};
-			this.fireCreateSwal(swal_object);
-		}
-	}
-	// mounted(){
-	// 	this.getUserRole();
-	// }
+    props: {
+        steps: Array,
+        endpoints: Object,
+    },
+    mixins: [form],
+    data() {
+        return {
+            name: "Create Mandate",
+            user: this.$store.state.user,
+            tabIndex: 0,
+            mandate: {
+                date: "",
+                position: "",
+                full_name: {
+                    last_name: "",
+                    first_name: "",
+                    middle_name: "",
+                },
+                region: "",
+                permanent_address: "",
+                present_address: "",
+                gender: "",
+                civil_status: "",
+                birthdate: "",
+                age: "",
+                mobile_number: "",
+                telephone_number: "",
+                religion: "",
+                sss_number: "",
+                tin_number: "",
+                pagibig_number: "",
+                philhealth_number: "",
+                passport_number: "",
+                tertiary_details: {
+                    tertiary_name: "",
+                    tertiary_date: "",
+                },
+                secondary_details: {
+                    secondary_name: "",
+                    secondary_date: "",
+                },
+                primary_details: {
+                    primary_name: "",
+                    primary_date: "",
+                },
+                work_details: [
+                    {
+                        work_position: "",
+                        job_description: "",
+                        start_date: "",
+                        end_date: "",
+                    },
+                ],
+                father_details: {
+                    father_name: "",
+                    father_occupation: "",
+                },
+                mother_details: {
+                    mother_name: "",
+                    mother_occupation: "",
+                },
+                spouse_details: {
+                    spouse_name: "",
+                    spouse_occupation: "",
+                },
+                emergency_details: [
+                    {
+                        contact_person: "",
+                        contact_number: "",
+                    },
+                ],
+            },
+        };
+    },
+    components: {
+        "general-info": generalInfo,
+        "government-details": governmentDetails,
+        "other-information": otherInformation,
+        "education-work": educationWork,
+    },
+    computed: {
+        ...mapState({
+            steps(state) {
+                console.log(state);
+                return state["mandate-create"].steps;
+            },
+        }),
+        isDisabled() {
+            if (this.mandate.position == "" || this.mandate.full_name == "") {
+                return true;
+            }
+            if (
+                this.mandate.present_address == "" ||
+                this.mandate.permanent_address == ""
+            ) {
+                return true;
+            }
+            if (
+                this.mandate.mobile_number == "" ||
+                this.mandate.telephone_number == ""
+            ) {
+                return true;
+            }
+            if (
+                this.mandate.tin_number == "" ||
+                this.mandate.sss_number == ""
+            ) {
+                return true;
+            }
+            if (
+                this.mandate.pagibig_number == "" ||
+                this.mandate.philhealth_number == ""
+            ) {
+                return true;
+            }
+        },
+    },
+    methods: {
+        createMandate() {
+            var swal_object = {
+                title: "Create Mandate",
+                icon: "question",
+                confirmButtonText: "Create Mandate",
+                text: "Please check the details provided.",
+                item: this.mandate,
+                endpoints: {
+                    api: "/api/mandate",
+                    show_route: "mandate_show",
+                },
+            };
+            this.fireCreateSwal(swal_object);
+        },
+    },
+    beforeCreate() {
+        this.$store.registerModule("mandate-create", mandateModule);
+        // this.registerStoreModule("account-create", accountModule);
+    },
+    mounted() {},
 };
 </script>
 
--- resources/js/store/modules/mandate.js
+++ resources/js/store/modules/mandate.js
@@ -1,10 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import axios from "axios";
+
 Vue.use(Vuex);
-export default new Vuex.Store({
+export default {
     namespaced: true,
     state: {
+        name: "Mandate",
+        model: "App\\Mandate",
+        mode: "Show",
+        item: null,
+
+        loading: true,
+        show_return_modal: false,
         steps: [
             {
                 name: "Create",
@@ -16,9 +25,83 @@ export default new Vuex.Store({
                 responsible: "Approver",
                 database_equivalent: ["For Approval", "Returned to Approver"],
             },
+            {
+                name: "Approve",
+                responsible: null,
+                database_equivalent: ["Approved"],
+            },
         ],
     },
-    getters: {},
-    mutations: {},
-    actions: {},
-});
+    getters: {
+        getItem(state) {
+            console.log("i wonder why", state.item);
+            return state.item;
+        },
+        getSteps(state) {
+            return state.steps;
+        },
+        getMode(state) {
+            return state.mode;
+        },
+        getEndpoints(state) {
+            var endpoints = {
+                api: "/api/mandate/" + state.item.id,
+            };
+
+            return endpoints;
+        },
+        getShowReturnModal(state) {
+            return state.show_return_modal;
+        },
+    },
+    mutations: {
+        storeItem(state, mandate) {
+            state.item = mandate;
+        },
+        changeMode(state, mode) {
+            console.log("pumasok ba");
+            state.mode = mode;
+        },
+        changeShowReturnModal(state, status) {
+            console.log("is something happening");
+            state.show_return_modal = status;
+            console.log(state.show_return_modal);
+        },
+    },
+    actions: {
+        changeMode(context, mode) {
+            // return new Promise((resolve, reject) => {
+            //     console.log('aitttttttttttttt');
+            //     console.log(id);
+            //     axios.get("/api/mandate/"+id).then(response => {
+            //         var mandate = response.data.data;
+            //         context.commit("storeMandate", mandate);
+            //         resolve(mandate);
+            //     }).catch(e => {
+            //         reject(e);
+            //     });
+            // });
+            context.commit("changeMode", mode);
+            if (mode == "Show") {
+                context.dispatch("storeMandate", context.state.mandate.id);
+            }
+        },
+        storeItem(context, id) {
+            return new Promise((resolve, reject) => {
+                console.log("aitttttttttttttt");
+                console.log(id);
+                axios
+                    .get("/api/mandate/" + id)
+                    .then((response) => {
+                        var mandate = response.data.data;
+                        console.log("am i being changed?", id);
+                        context.commit("storeItem", mandate);
+                        resolve(mandate);
+                    })
+                    .catch((e) => {
+                        reject(e);
+                    });
+            });
+        },
+    },
+};
